7cf14fa5c2135aac87ae8f993b4f09f46b450720
SheillaO <sheilla.alusa@strathmore.edu>
2026-05-06 12:27:10 +0300

    Update data.js

--- data/data.js
+++ data/data.js
@@ -2,6 +2,7 @@ export const data = [
   
   {
     name: "Hobbiton Movie Set",
+    image: "https://images.unsplash.com/photo-1598011394059-74ea7b84cfc7?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     location: "Matamata",
     country: "New Zealand",
     continent: "Oceania",
